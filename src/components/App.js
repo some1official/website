49c65506203f4f03f2f9a0a23c7b1be816449dd5
nottimnik <timnikofficial@gmail.com>
2021-03-23 22:17:49 +0100

    Added Log In

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,30 @@ import React from 'react'
 import Signup from './Signup'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import Login from './Login';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container class="d-flex align-items-center justify-content-center"
-        style={{minHeight: "100vh"}}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    
+    <Container className="d-flex align-items-center justify-content-center"
+      style={{minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+              <Route exact path="/" component={Home} />
+              <Route path="/signup" component={Signup}/>
+              <Route path="/login" component={Login}/>
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   )
 }
 
