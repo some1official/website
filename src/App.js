4a40028b31f2a659037e0e0f6e4f1200afef9ffc
timnik <>
2021-05-28 21:29:25 +0000

    Created first tutorial

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 //Pages
 import Home from './pages/Home'
 import TemplatePost from './components/Posts/TemplatePost/TemplatePost'
+import TutorialPythonIntro from './pages/tutorials/python/tutorial/TutorialPythonIntro'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={TemplatePost} />
+        <Route path="/" component={TutorialPythonIntro} />
         <Route path="/template" component={TemplatePost} />
       </Switch>
     </BrowserRouter>
---
+++ src/pages/tutorials/python/tutorial/TutorialPythonIntro.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './../../../../style/pages/tutorials/python/base.css'
+
+export default function tutorialPython() {
+    return (
+        <>
+            <div className="content-tutorialPython">
+              <h1 className="title-content-tutorialPython">Python Introduction</h1>
+              <hr className="hr-content-tutorialPython"/>
+
+              <h1 className="big-title-content-tutorialPython">What is Python?</h1>
+              <p className="text-content-tutorialPython">
+              Python is a very popular high-level programming language, because of the easy to understand syntax
+              is considered one of the easiest programming languages to learn. It was created by by Guido van Rossum, 
+              and released in 1991.
+              </p>
+
+              <br/><br/>
+
+              <h1 className="big-title-content-tutorialPython">What can Python do?</h1>
+
+              <ul>
+                <li className="li-content-tutorialPython">
+                  <p className="text-content-tutorialPython">can we be used to create web applications</p>
+                </li>
+                <li className="li-content-tutorialPython">
+                  <p className="text-content-tutorialPython">can be used to automate stuff</p>
+                </li>
+                <li className="li-content-tutorialPython">
+                  <p className="text-content-tutorialPython">can be used alongside software to create workflows.</p>
+                </li>
+                <li className="li-content-tutorialPython">
+                  <p className="text-content-tutorialPython">for rapid prototyping, or for production-ready software development.</p>
+                </li>
+              </ul>
+            </div>
+        </>
+    )
+}
